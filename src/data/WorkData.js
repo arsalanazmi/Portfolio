f6d6a88a09960cc959a61d33acb41ed5e5ff5261
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2022-04-09 17:09:39 +0500

    added quiz project

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,6 +1,15 @@
 export const Work = [
   {
     id: 1,
+    name: "React Quiz App",
+    description: "Quiz App with the progress bar",
+    tags: ["React", "Styled Components"],
+
+    demo: "https://quiz-app-progress.netlify.app/",
+    github: "https://github.com/arsalanazmi/React-Quiz-App",
+  },
+  {
+    id: 2,
     name: "Login Authentication App",
     description: "MERN Stack User Authentication App",
     tags: [
@@ -12,99 +21,99 @@ export const Work = [
       "Express",
       "MongoDB",
       "Bcrypt",
-      "JsonWebToken"
+      "JsonWebToken",
     ],
 
     demo: "https://loginauth-app.herokuapp.com/",
-    github: "https://github.com/arsalanazmi/Login-Auth-App"
+    github: "https://github.com/arsalanazmi/Login-Auth-App",
   },
   {
-    id: 2,
+    id: 3,
     name: "Student App",
     description: "MERN Stack CRUD Student App",
     tags: ["React", "Bootstrap", "Node", "Express", "MongoDB"],
 
     demo: "https://student-record-app.herokuapp.com/",
-    github: "https://github.com/arsalanazmi/Student-App"
+    github: "https://github.com/arsalanazmi/Student-App",
   },
   {
-    id: 3,
+    id: 4,
     name: "Task Box",
     description:
       "Todo App with display filter options of all, completed & incomplete tasks",
     tags: ["React", "TypeScript"],
 
     demo: "http://bootcamp2020_taskbox.surge.sh/",
-    github: "https://github.com/arsalanazmi/Taskbox"
+    github: "https://github.com/arsalanazmi/Taskbox",
   },
   {
-    id: 4,
+    id: 5,
     name: "Multi Step Form",
     description: "React Multi Step Form.",
     tags: ["React", "TypeScript", "Material-Ui", "Formik", "Yup"],
 
     demo: "https://multiplestep-form.netlify.app/",
-    github: "https://github.com/arsalanazmi/Multi-Step-Form"
+    github: "https://github.com/arsalanazmi/Multi-Step-Form",
   },
   {
-    id: 5,
+    id: 6,
     name: "Shopping Basket",
     description:
       "An Ecommerce shoe website (Only Frontend. No payment method).",
     tags: ["React", "Material-Ui", "Redux-Toolkit"],
 
     demo: "https://nike-shoe-shopping-basket.netlify.app/",
-    github: "https://github.com/arsalanazmi/Shopping-Basket"
+    github: "https://github.com/arsalanazmi/Shopping-Basket",
   },
   {
-    id: 6,
+    id: 7,
     name: "Expense-Tracker-PWA-Web-App",
     description:
       "A Pwa expense tracker app which can show your balance with the history of all the transactions.",
     tags: ["React", "TypeScript", "Material-Ui"],
 
     demo: "https://timer-application.netlify.app/",
-    github: "https://github.com/arsalanazmi/Timer-App"
+    github: "https://github.com/arsalanazmi/Timer-App",
   },
   {
-    id: 7,
+    id: 8,
     name: "Quiz App",
     description: "A Quiz App with the user defined difficulty level & subject.",
     tags: ["React", "TypeScript", "Material-Ui", "Styled Components"],
 
     demo: "http://tricky_quiz.surge.sh/",
-    github: "https://github.com/arsalanazmi/Quiz-App"
+    github: "https://github.com/arsalanazmi/Quiz-App",
   },
   {
-    id: 8,
+    id: 9,
     name: "Tiny-Short-Web-Animations",
     description:
       "A single web page with the text coming in the animated fashion.",
     tags: ["React", "Material-Ui", "Aos", "Typewriter-effect"],
 
     demo: "http://tiny_short_web_animations.surge.sh/",
-    github: "https://github.com/arsalanazmi/Tiny-Short-Web-Animations"
+    github: "https://github.com/arsalanazmi/Tiny-Short-Web-Animations",
   },
   {
-    id: 9,
+    id: 10,
     name: "Web-Animations",
     description: "An animated scene whose speed increases on mouse click.",
     tags: ["React", "Material-Ui", "@wellyshen/use-web-animations"],
 
     demo: "http://tiny_short_web_animations.surge.sh/",
-    github: "https://github.com/arsalanazmi/Web-Animations"
+    github: "https://github.com/arsalanazmi/Web-Animations",
   },
   {
-    id: 10,
+    id: 11,
     name: "Nike-Shoe-Store",
     description: "An online Nike Shoe Store web template.",
     tags: ["React", "Material-Ui", "bootstrap"],
 
     demo: "http://nike_shoe_store.surge.sh/",
-    github: "https://github.com/arsalanazmi/Nike-Shoe-Store"
+    github: "https://github.com/arsalanazmi/Nike-Shoe-Store",
   },
   {
-    id: 11,
+    id: 12,
     name: "Covid-19-Tracker-App",
     description: "Covid19 Global Stats Tracker with the help of chart.js.",
     tags: [
@@ -112,28 +121,28 @@ export const Work = [
       "Bootstrap",
       "Chart.js",
       "React-Loader-Spinner",
-      "React-Countup"
+      "React-Countup",
     ],
 
     demo: "http://covid-19_stats_tracker_app.surge.sh/",
-    github: "https://github.com/arsalanazmi/Covid-19-Tracker-App"
+    github: "https://github.com/arsalanazmi/Covid-19-Tracker-App",
   },
   {
-    id: 12,
+    id: 13,
     name: "Online-Shopping",
     description: "An online shopping store web template.",
     tags: ["React", "Bootstrap"],
 
     demo: "https://online-shoppping.netlify.app/",
-    github: "https://github.com/arsalanazmi/Online-Shopping"
+    github: "https://github.com/arsalanazmi/Online-Shopping",
   },
   {
-    id: 13,
+    id: 14,
     name: "Restaurent-Template",
     description: "A sample restaurent web template with some animations.",
     tags: ["HTML", "CSS", "JavaScript", "Jquery"],
 
     demo: "https://eatery-cafe.netlify.app/",
-    github: "https://github.com/arsalanazmi/Restaurent-Template"
-  }
+    github: "https://github.com/arsalanazmi/Restaurent-Template",
+  },
 ];
